d710f7efd432a85f041e1783361caeb04b98791d
Aleksandra <ab0200@students.necsd.net>
2021-01-22 07:43:31 +0000

    prob 3

--- Prob3.java
+++ Prob3.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 class Prob3 {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("enter a word:");
+
+    String word = scan.nextLine();
+    word = word.toLowerCase();
+
+    System.out.println("enter another word:");
+    String word2 = scan.nextLine();
+    word2 = word2.toLowerCase();
+
+    
+
+
+
     
   }
 }
