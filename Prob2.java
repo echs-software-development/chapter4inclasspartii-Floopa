ebdcde2f4139570522d120ec59450fd0310470b5
Aleksandra <ab0200@students.necsd.net>
2021-01-22 07:38:08 +0000

    prob 2

--- Prob2.java
+++ Prob2.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 
 class Prob2 {
   public static void main(String[] args) {
+    //Declare a scanner and input a String
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter a string:");
+    String h = scan.nextLine();
+
+    //Print first charcter
+    System.out.println(h.substring(0,1));
+
+    //Print last charcter 
+    System.out.println(h.substring(h.length()-1));
+
+
     
   }
 }
